d0a18dc65ff475bb20289edf754bbdf740e310f3
doraly <doraly9110@hotmail.com>
2022-04-26 14:33:55 -0500

    /bin/python3 /home/mint/Escritorio/Programate/battle/Phyton/snake/Snake.py

--- Snake.py
+++ Snake.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+import time 
 
 #crear un objeto del escenario
 screen = Screen()
@@ -6,14 +7,47 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Programate snake")
 
+#desactive la animacion
+screen.tracer(0)
+
 #creacion cuerpo de la serpiente
 
-positions = [(0,0),(-20,0),(-40,0)]
+starting_positions = [(0,0),(-20,0),(-40,0)]
 
-for position in positions:
+segments =[]
+
+for position in starting_positions:
     snake_segment = Turtle("square")# creo objeto serpiente
     snake_segment.color("white")
+    snake_segment.penup()# quitar la linea
     snake_segment.goto(position)#cordenadas x,y  en y =0
+    segments.append(snake_segment)#agregar un elemento a lista
+
+#estado juego
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    #aumentar velocidad
+    time.sleep(0.2)
+
+    '''
+    for segment in segments:
+        segment.forward(20)# cada segmento se vaya 20 esp
+        segment[0].left(90)    
+
+    '''
+
+    for seg_num in range(len(segments) -1, 0, -1 ):
+        new_x = segments[seg_num -1].xcor()
+        new_y = segments[seg_num -1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+
+
+    segments[0].forward(20)
+    segments[0].left(90) 
+
+
 '''
 snake_segment = Turtle("square")# creo objeto serpiente
 snake_segment.color("white")
