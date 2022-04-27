ed1f4ba8cc392e0ca6849ef78cfb633e225dcb56
doraly <doraly9110@hotmail.com>
2022-04-27 12:20:19 -0500

    creando constantes para remplazar por los grados

--- snake.py
+++ snake.py
@@ -2,13 +2,18 @@ from turtle import  Turtle
 
 #creacion cuerpo de la serpiente
 STARTING_POSITIONS = [(0,0),(-20,0),(-40,0)]
+DOWN = 270
+UP = 90
+RIGHT = 0
+LEFT = 180
+
 
 class Snake:
 
         def __init__(self):
             self.segments = []#atributo
             self.create_snake()#metodo crear serpiente
-
+            self.head = self.segments[0]#almacena la cabeza
 
         def create_snake(self):
             for position in STARTING_POSITIONS:
@@ -26,20 +31,24 @@ class Snake:
                 self.segments[seg_num].goto(new_x, new_y)
 
 
-            self.segments[0].forward(20)
+            self.head.forward(20)
             #segments[0].left(90)
 
         def up(self):
-            self.segments[0].setheading(90)
-
-        def right(self):
-            self.segments[0].setheading(0)
+            if self.head.heading() != DOWN:
+                self.head.setheading(UP)
 
         def down(self):
-            self.segments[0].setheading(270)  
+            if self.head.heading() != UP:
+                self.head.setheading(DOWN)
 
         def left (self):
-            self.segments[0].setheading(180)     
+            if self.head.heading() != RIGHT:
+                self.head.setheading(LEFT) 
+
+        def right(self):
+            if self.head.heading() != LEFT:
+                self.head.setheading(RIGHT)
 
         '''
         snake_segment = Turtle("square")# creo objeto serpiente
