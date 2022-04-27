c40dae8b31a3a9e64df925a663942ba9c814adfd
doraly <doraly9110@hotmail.com>
2022-04-27 12:51:09 -0500

    genera la comida aleatoria

--- food.py
+++ food.py
@@ -1,6 +1,7 @@
 from turtle import  Turtle
 
 #libreria  random que  genera cosas aleatorias
+import random
 #comida hereda de turtle
 class Food(Turtle):#heredad todos los metodos que tiene turtle
 
@@ -11,5 +12,10 @@ class Food(Turtle):#heredad todos los metodos que tiene turtle
         self.penup()#linea
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("green")
+        self.refresh()# cada ves se refresque
 
-
+    #aparezca la comida distos lugares
+    def refresh(self):
+        random_x  = random.randint(-200, 200)#genera un numero entero -200 a 200
+        random_y = random.randint(-200, 200)
+        self.goto(random_x,random_y)
